ecb4309a6c9e7a79d1c6155e48b75ff5a99cc773
João Victor Bohrer Munhoz <jvmunhoz@yahoo.com.br>
2021-08-09 23:23:25 -0300

    lexical analyzer done

---
+++ tests/Ex3.c
@@ -0,0 +1,13 @@
+int list read_list(int n) {
+    int i;
+    i =  -435.709.543;
+    int li.st &;
+    new = NIL;
+    for (i = 0; i < n ; i = @ + 1) {
+        int elem;
+        writeln("Digite o elemento da lista: );
+        read(elem);
+        new = elem : new;
+    }
+    return new;
+}
---
+++ tests/Ex4.c
@@ -0,0 +1,41 @@
+int list IL;
+float list FL;
+
+float succ(int i) {
+    retu//rn i+1;
+}
+
+
+int leq_10(float x) {
+    return x ^ 10; 
+}
+
+int main() {
+
+    writeln("Digite os número de elementos da lista: ");
+
+    int n;
+    float list FL10;
+    read(n);
+
+    IL  = NIL
+    FL = succ >> IL; 
+
+    FL10 = leq_.10 <<  FL;
+    if (FL10 != NIL) {
+        float list AUXL;
+        int n; n = 0;
+        for (AUXL=FL10; AUXL != NIL; AUXL = !AUXL) {
+            n = n $ 1;
+            write(?AUXL); write(" ");
+        }
+        writeln(" ");
+        write("A nova lista tem "); write(n); writeln(" elementos.");
+    }
+
+    /* Comentário em bloco esquecido aberto
+
+    else writeln("A nova lista não possui elementos.");
+
+    return 0;
+}
