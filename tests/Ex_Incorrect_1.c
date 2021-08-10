8f3a51507e8475965f2f2c5e68f581cfbc117647
João Victor Bohrer Munhoz <jvmunhoz@yahoo.com.br>
2021-08-10 12:17:15 -0300

    renamed test files

---
+++ tests/Ex_Incorrect_1.c
@@ -0,0 +1,13 @@
+int list read_list(int n) {
+    int i;
+    i =  -435.709.543;
+    int list;
+    new = NIL;
+    for (i = 0; i < n ; i = $i + 1) {
+        int elem;
+        writeln("Digite o elemento da lista: );
+        read(elem);
+        new = elem : new;
+    }
+    return new;
+}
